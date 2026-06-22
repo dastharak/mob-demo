b6b9254ef4bcdfc6d41e55e7b0ba19143ca19611
Dasun <sendtodasun@gmail.com>
2026-06-22 18:13:44 +0530

    minor update to callback log entry id:comment

--- LifeCycle/app/src/main/java/com/github/dastharak/lifecycle/CallbackLog.kt
+++ LifeCycle/app/src/main/java/com/github/dastharak/lifecycle/CallbackLog.kt
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger
 private val counter = AtomicInteger(1)
 
 data class CallbackLog(
-    val id: Int = counter.getAndIncrement(),//UUID.randomUUID().toString(),
+    val id: Int = counter.getAndIncrement(),// Get the current then increment
     val message: String,
     val timestamp: String
 )
